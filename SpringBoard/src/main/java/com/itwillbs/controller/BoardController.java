3739375056883270c500bea39e4d1a47a15e8354
유지인 <jiin4083@naver.com>
2024-02-21 16:38:58 +0900

    20240221 registerPOST() 완료

--- SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
@@ -1,24 +1,58 @@
 package com.itwillbs.controller;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.itwillbs.domain.BoardVO;
+import com.itwillbs.service.BoardService;
+
 @Controller
 @RequestMapping(value = "/board/*")
 public class BoardController {
 	
+	// 서비스 객체 주입
+	@Inject
+	private BoardService bService;
+	
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
-	// 글쓰기 GET : /board/register  http://localhost:8088/board/register
+	// http://localhost:8088/board/register
+	// 글쓰기 GET : /board/register  
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public void registerGET() throws Exception{
 		logger.debug(" registerGET() 호출 ");
 		logger.debug(" /board/register.jsp 뷰 연결 ");
 	}
 	
-
+	// 글쓰기 POST : /board/register
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerPOST(BoardVO vo) throws Exception{
+		logger.debug(" registerPOST() 호출 ");
+		// 한글처리 필터했기 때문에 생략
+		// 전달정보(글 정보) 저장
+		logger.debug(" 전달정보 : "+vo);
+		// 서비스 -> DAO 글쓰기 동작 호출 
+		bService.regist(vo);
+		logger.debug(" 글쓰기 완료 ! >> list.jsp로 이동 ");
+		// 페이지 이동(list)
+		return "redirect:/board/list";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- SpringBoard/src/main/webapp/WEB-INF/views/board/register.jsp
+++ SpringBoard/src/main/webapp/WEB-INF/views/board/register.jsp
@@ -18,15 +18,15 @@
 		<div class="box-body">
 			<div class="form-group">
 				<label for="exampleInputEmail1">제  목</label>
-				<input type="text" class="form-control" id="title" placeholder="제목 뭐 할래">
+				<input type="text" class="form-control" id="title" placeholder="제목 뭐 할래" name="title">
 			</div>
 			<div class="form-group">
 				<label for="exampleInputPassword1">이  름</label> 
-				<input type="text" class="form-control" id="writer" placeholder="너 누구야">
+				<input type="text" class="form-control" id="writer" placeholder="너 누구야" name="writer">
 			</div>
 			<div class="form-group">
 				<label>내  용</label>
-				<textarea class="form-control" rows="3" id="content" placeholder="내용 입력해보자 ..."></textarea>
+				<textarea class="form-control" rows="3" id="content" placeholder="내용 입력해보자 ..." name="content"></textarea>
 			</div>
 		</div>
 
